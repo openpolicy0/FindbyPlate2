7e5dd74954d32d2aee4d0c92634dcd62238276c1
Don't feel bad <116130475+openpolicy0@users.noreply.github.com>
2023-06-27 05:23:08 -0400

    Add files via upload

---
+++ plate/plate_search.py
@@ -0,0 +1,44 @@
+import requests
+import sys
+import os
+import time
+
+from tqdm import tqdm
+from rich import print
+from bs4 import BeautifulSoup
+from time import sleep
+
+def plate():
+    pwd = os.getcwd()
+    with open(pwd+"/plate/plate", "r") as lp:
+         plate = lp.readline()
+
+    with open(pwd+"/plate/state", "r") as st:
+         state = st.readline()
+
+    url2 = 'https://findbyplate.com/US/'+state+'/'+plate+'/'
+
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 13; SM-T220) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    }
+
+    rs = requests.get(url2, headers=headers)
+    report = BeautifulSoup(rs.text, "html.parser")
+    sleep(0.2)
+    for _ in tqdm(range(100),
+
+       desc = "loading report...",
+       ascii = False,ncols=250):
+       time.sleep(0.1)
+    time.sleep(1)
+    print("[bold green]results found![/bold green]")
+    time.sleep(4)
+    tag_plate = report.find_all(["div", "/div"], class_="cell" or "clearfix")
+    try:
+        for tag_plate in tag_plate:
+            print("[bold blue]=============================================================================[/bold blue]")
+            print("[dim cyan][INFO]====>[/dim cyan][bold white]"+tag_plate.string+"[/bold white]")
+            sleep(1)
+        print("[bold blue]=============================================================================[/bold blue]")
+    except TypeError:
+            print("")
