2b58b04311632e2e178c8eeb87f63851a5971ac4
Don't feel bad <116130475+openpolicy0@users.noreply.github.com>
2024-03-07 19:35:43 -0500

    Add files via upload

--- plate/plate_search.py
+++ plate/plate_search.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import os
 import time
+import random
 
 from tqdm import tqdm
 from rich import print
@@ -25,20 +26,19 @@ def plate():
     rs = requests.get(url2, headers=headers)
     report = BeautifulSoup(rs.text, "html.parser")
     sleep(0.2)
-    for _ in tqdm(range(100),
 
-       desc = "loading report...",
-       ascii = False,ncols=100):
-       time.sleep(0.1)
-    time.sleep(1)
-    print("[bold green]results found![/bold green]")
+    animation = "|/-\\"
+    for i in range(200):
+        time.sleep(0.1)
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+
+    print("[bold green]\nDone![/bold green]")
     time.sleep(4)
     tag_plate = report.find_all(["div", "/div"], class_="cell" or "clearfix")
     try:
         for tag_plate in tag_plate:
-            print("[bold blue]=============================================================================[/bold blue]")
             print("[dim cyan][INFO]====>[/dim cyan][bold white]"+tag_plate.string+"[/bold white]")
-            sleep(1)
-        print("[bold blue]=============================================================================[/bold blue]")
+            sleep(0.2)
     except TypeError:
             print("")
--- plate/vin_search.py
+++ plate/vin_search.py
@@ -18,21 +18,20 @@ def vin():
     r = requests.get(url)
     html = BeautifulSoup(r.text, "html.parser")
     sleep(0.2)
-    for _ in tqdm(range(100),
 
-        desc = "loading report...",
-        ascii = False,ncols=100):
+    animation = "|/-\\"
+
+    for i in range(200):
         time.sleep(0.1)
-    time.sleep(1)
-    print("[bold green]results found![/bold green]")
-    sleep(4)
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+
+    print("[bold green]\nDone![/bold green]")
+    time.sleep(4)
     tag1 = html.find(["p"], class_="h2 subtitle-2")
-    print("[bold blue]=========================================================[/bold blue]")
-    print("[dim cyan][INFO]====>[/dim cyan][bold white]"+tag1.string+"[/bold white]")
+    print("[dim cyan][INFO][CAR NAME]====>[/dim cyan][bold white]"+tag1.string+"[/bold white]")
 
     tag2 = html.find_all(["td", "div"], class_="txt")
     for tag2 in tag2:
-        print("[bold blue]=========================================================[/bold blue]")
         print("[dim cyan][INFO]====>[/dim cyan][bold white]"+tag2.string+"[/bold white]")
-        sleep(1)
-    print("[bold blue]=========================================================[/bold blue]")
+        sleep(0.2)
